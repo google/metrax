c280a54af5c17104abe5c8b4f8f6f4b00167f7b3
Jiwon Shin <jiwonshin@google.com>
2025-04-07 19:53:28 +0000

    modify docstring

--- src/metrax/metrax_test.py
+++ src/metrax/metrax_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for metrax NNX metrics."""
+"""Tests for metrax metrics."""
 from absl.testing import absltest
 from absl.testing import parameterized
 import jax
