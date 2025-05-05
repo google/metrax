b96bfeee7f580742168e6e5aeab9ff482d63d2e5
Jiwon Shin <jiwonshin@google.com>
2025-05-05 07:38:39 +0000

    revert ranking metrics changes

--- src/metrax/ranking_metrics_test.py
+++ src/metrax/ranking_metrics_test.py
@@ -12,269 +12,154 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for metrax regression metrics."""
-
-import os
-os.environ['KERAS_BACKEND'] = 'jax'
+"""Tests for metrax ranking metrics."""
 
 from absl.testing import absltest
 from absl.testing import parameterized
-import jax
 import jax.numpy as jnp
-import keras
+import keras_rs
 import metrax
 import numpy as np
-from sklearn import metrics as sklearn_metrics
 
 np.random.seed(42)
-BATCHES = 4
-BATCH_SIZE = 8
+BATCH_SIZE = 4
+VOCAB_SIZE = 8
 OUTPUT_LABELS = np.random.randint(
     0,
     2,
-    size=(BATCHES, BATCH_SIZE),
+    size=(BATCH_SIZE, VOCAB_SIZE),
 ).astype(np.float32)
-OUTPUT_PREDS = np.random.uniform(size=(BATCHES, BATCH_SIZE))
-OUTPUT_PREDS_F16 = OUTPUT_PREDS.astype(jnp.float16)
-OUTPUT_PREDS_F32 = OUTPUT_PREDS.astype(jnp.float32)
-OUTPUT_PREDS_BF16 = OUTPUT_PREDS.astype(jnp.bfloat16)
-OUTPUT_LABELS_BS1 = np.random.randint(
+OUTPUT_PREDS = np.random.uniform(size=(BATCH_SIZE, VOCAB_SIZE)).astype(
+    np.float32
+)
+OUTPUT_RELEVANCES = np.random.randint(
+    0,
+    4,
+    size=(BATCH_SIZE, VOCAB_SIZE),
+).astype(np.float32)
+OUTPUT_LABELS_VS1 = np.random.randint(
     0,
     2,
-    size=(BATCHES, 1),
+    size=(BATCH_SIZE, 1),
 ).astype(np.float32)
-OUTPUT_PREDS_BS1 = np.random.uniform(size=(BATCHES, 1)).astype(np.float32)
-SAMPLE_WEIGHTS = np.tile(
-    [0.5, 1, 0, 0, 0, 0, 0, 0],
-    (BATCHES, 1),
+OUTPUT_PREDS_VS1 = np.random.uniform(size=(BATCH_SIZE, 1)).astype(np.float32)
+OUTPUT_RELEVANCES_VS1 = np.random.randint(
+    0,
+    4,
+    size=(BATCH_SIZE, 1),
 ).astype(np.float32)
 
 
-class RegressionMetricsTest(parameterized.TestCase):
+class RankingMetricsTest(parameterized.TestCase):
 
-  def test_multiple_devices(self):
-    """Test that metrax metrics work across multiple devices using R2 as an example."""
+  @parameterized.named_parameters(
+      (
+          'averageprecisionatk_basic',
+          metrax.AveragePrecisionAtK,
+          keras_rs.metrics.MeanAveragePrecision,
+          OUTPUT_LABELS,
+          OUTPUT_PREDS,
+      ),
+      (
+          'averageprecisionatk_vocab_size_one',
+          metrax.AveragePrecisionAtK,
+          keras_rs.metrics.MeanAveragePrecision,
+          OUTPUT_LABELS_VS1,
+          OUTPUT_PREDS_VS1,
+      ),
+      (
+          'precisionatk_basic',
+          metrax.PrecisionAtK,
+          keras_rs.metrics.PrecisionAtK,
+          OUTPUT_LABELS,
+          OUTPUT_PREDS,
+      ),
+      (
+          'precisionatk_vocab_size_one',
+          metrax.PrecisionAtK,
+          keras_rs.metrics.PrecisionAtK,
+          OUTPUT_LABELS_VS1,
+          OUTPUT_PREDS_VS1,
+      ),
+      (
+          'recallatk_basic',
+          metrax.RecallAtK,
+          keras_rs.metrics.RecallAtK,
+          OUTPUT_LABELS,
+          OUTPUT_PREDS,
+      ),
+      (
+          'recallatk_vocab_size_one',
+          metrax.RecallAtK,
+          keras_rs.metrics.RecallAtK,
+          OUTPUT_LABELS_VS1,
+          OUTPUT_PREDS_VS1,
+      ),
+      (
+          'dcgatk_basic',
+          metrax.DCGAtK,
+          keras_rs.metrics.DCG,
+          OUTPUT_RELEVANCES,
+          OUTPUT_PREDS,
+      ),
+      (
+          'dcgatk_vocab_size_one',
+          metrax.DCGAtK,
+          keras_rs.metrics.DCG,
+          OUTPUT_RELEVANCES_VS1,
+          OUTPUT_PREDS_VS1,
+      ),
+      (
+          'ndcgatk_basic',
+          metrax.NDCGAtK,
+          keras_rs.metrics.NDCG,
+          OUTPUT_RELEVANCES,
+          OUTPUT_PREDS,
+      ),
+      (
+          'ndcgatk_vocab_size_one',
+          metrax.NDCGAtK,
+          keras_rs.metrics.NDCG,
+          OUTPUT_RELEVANCES_VS1,
+          OUTPUT_PREDS_VS1,
+      ),
+      (
+          'mrr_basic',
+          metrax.MRR,
+          keras_rs.metrics.MeanReciprocalRank,
+          OUTPUT_LABELS,
+          OUTPUT_PREDS,
+      ),
+      (
+          'mrr_vocab_size_one',
+          metrax.MRR,
+          keras_rs.metrics.MeanReciprocalRank,
+          OUTPUT_LABELS_VS1,
+          OUTPUT_PREDS_VS1,
+      ),
+  )
+  def test_ranking_metrics(self, metric, keras_metric, y_true, y_pred):
+    """Test that a metrax ranking metric computes correct values."""
+    ks = jnp.array([1, 2, 3, 4, 5, 6])
+    metric = metric.from_model_output(
+        predictions=y_pred,
+        labels=y_true,
+        ks=ks,
+    )
 
-    def create_r2(logits, labels):
-      """Creates a metrax RSQUARED metric given logits and labels."""
-      return metrax.RSQUARED.from_model_output(logits, labels)
+    keras_metrics = [keras_metric(k=n+1) for n in range(6)]
+    results = []
+    for keras_metric in keras_metrics:
+       keras_metric.update_state(y_true, y_pred)
+       results.append(keras_metric.result())
 
-    def sharded_r2(logits, labels):
-      """Calculates sharded R2 across devices."""
-      num_devices = jax.device_count()
-
-      shard_size = logits.shape[0] // num_devices
-      sharded_logits = logits.reshape(num_devices, shard_size, logits.shape[-1])
-      sharded_labels = labels.reshape(num_devices, shard_size, labels.shape[-1])
-
-      r2_for_devices = jax.pmap(create_r2)(sharded_logits, sharded_labels)
-      return r2_for_devices
-
-    y_pred = OUTPUT_PREDS
-    y_true = OUTPUT_LABELS
-    metric = jax.jit(sharded_r2)(y_pred, y_true)
-    metric = metric.reduce()
-
-    keras_r2 = keras.metrics.R2Score()
-    for labels, logits in zip(y_true, y_pred):
-      keras_r2.update_state(
-          labels[:, jnp.newaxis],
-          logits[:, jnp.newaxis],
-      )
-    expected = keras_r2.result()
     np.testing.assert_allclose(
         metric.compute(),
-        expected,
+        jnp.array(results),
         rtol=1e-05,
         atol=1e-05,
     )
 
-  def test_mse_empty(self):
-    """Tests the `empty` method of the `MSE` class."""
-    m = metrax.MSE.empty()
-    self.assertEqual(m.total, jnp.array(0, jnp.float32))
-    self.assertEqual(m.count, jnp.array(0, jnp.int32))
-
-  def test_rmse_empty(self):
-    """Tests the `empty` method of the `RMSE` class."""
-    m = metrax.RMSE.empty()
-    self.assertEqual(m.total, jnp.array(0, jnp.float32))
-    self.assertEqual(m.count, jnp.array(0, jnp.int32))
-
-  def test_rsquared_empty(self):
-    """Tests the `empty` method of the `RSQUARED` class."""
-    m = metrax.RSQUARED.empty()
-    self.assertEqual(m.total, jnp.array(0, jnp.float32))
-    self.assertEqual(m.count, jnp.array(0, jnp.float32))
-    self.assertEqual(m.sum_of_squared_error, jnp.array(0, jnp.float32))
-    self.assertEqual(m.sum_of_squared_label, jnp.array(0, jnp.float32))
-
-  @parameterized.named_parameters(
-      ('basic_f16', OUTPUT_LABELS, OUTPUT_PREDS_F16, None),
-      ('basic_f32', OUTPUT_LABELS, OUTPUT_PREDS_F32, None),
-      ('basic_bf16', OUTPUT_LABELS, OUTPUT_PREDS_BF16, None),
-      ('batch_size_one', OUTPUT_LABELS_BS1, OUTPUT_PREDS_BS1, None),
-      ('weighted_f16', OUTPUT_LABELS, OUTPUT_PREDS_F16, SAMPLE_WEIGHTS),
-      ('weighted_f32', OUTPUT_LABELS, OUTPUT_PREDS_F32, SAMPLE_WEIGHTS),
-      ('weighted_bf16', OUTPUT_LABELS, OUTPUT_PREDS_BF16, SAMPLE_WEIGHTS),
-  )
-  def test_mae(self, y_true, y_pred, sample_weights):
-    """Test that `MAE` Metric computes correct values."""
-    y_true = y_true.astype(y_pred.dtype)
-    y_pred = y_pred.astype(y_true.dtype)
-    if sample_weights is None:
-      sample_weights = np.ones_like(y_true)
-
-    metric = None
-    for labels, logits, weights in zip(y_true, y_pred, sample_weights):
-      update = metrax.MAE.from_model_output(
-          predictions=logits,
-          labels=labels,
-          sample_weights=weights,
-      )
-      metric = update if metric is None else metric.merge(update)
-
-    # TODO(jiwonshin): Use `keras.metrics.MeanAbsoluteError` once it supports
-    # sample weights.
-    expected = sklearn_metrics.mean_absolute_error(
-        y_true.flatten(),
-        y_pred.flatten(),
-        sample_weight=sample_weights.flatten(),
-    )
-    # Use lower tolerance for lower precision dtypes.
-    rtol = 1e-2 if y_true.dtype in (jnp.float16, jnp.bfloat16) else 1e-05
-    atol = 1e-2 if y_true.dtype in (jnp.float16, jnp.bfloat16) else 1e-05
-    np.testing.assert_allclose(
-        metric.compute(),
-        expected,
-        rtol=rtol,
-        atol=atol,
-    )
-
-  @parameterized.named_parameters(
-      ('basic_f16', OUTPUT_LABELS, OUTPUT_PREDS_F16, None),
-      ('basic_f32', OUTPUT_LABELS, OUTPUT_PREDS_F32, None),
-      ('basic_bf16', OUTPUT_LABELS, OUTPUT_PREDS_BF16, None),
-      ('batch_size_one', OUTPUT_LABELS_BS1, OUTPUT_PREDS_BS1, None),
-      ('weighted_f16', OUTPUT_LABELS, OUTPUT_PREDS_F16, SAMPLE_WEIGHTS),
-      ('weighted_f32', OUTPUT_LABELS, OUTPUT_PREDS_F32, SAMPLE_WEIGHTS),
-      ('weighted_bf16', OUTPUT_LABELS, OUTPUT_PREDS_BF16, SAMPLE_WEIGHTS),
-  )
-  def test_mse(self, y_true, y_pred, sample_weights):
-    """Test that `MSE` Metric computes correct values."""
-    y_true = y_true.astype(y_pred.dtype)
-    y_pred = y_pred.astype(y_true.dtype)
-    if sample_weights is None:
-      sample_weights = np.ones_like(y_true)
-
-    metric = None
-    for labels, logits, weights in zip(y_true, y_pred, sample_weights):
-      update = metrax.MSE.from_model_output(
-          predictions=logits,
-          labels=labels,
-          sample_weights=weights,
-      )
-      metric = update if metric is None else metric.merge(update)
-
-    # TODO(jiwonshin): Use `keras.metrics.MeanSquaredError` once it supports
-    # sample weights.
-    expected = sklearn_metrics.mean_squared_error(
-        y_true.flatten(),
-        y_pred.flatten(),
-        sample_weight=sample_weights.flatten(),
-    )
-    # Use lower tolerance for lower precision dtypes.
-    rtol = 1e-2 if y_true.dtype in (jnp.float16, jnp.bfloat16) else 1e-05
-    atol = 1e-2 if y_true.dtype in (jnp.float16, jnp.bfloat16) else 1e-05
-    np.testing.assert_allclose(
-        metric.compute(),
-        expected,
-        rtol=rtol,
-        atol=atol,
-    )
-
-  @parameterized.named_parameters(
-      ('basic_f16', OUTPUT_LABELS, OUTPUT_PREDS_F16, None),
-      ('basic_f32', OUTPUT_LABELS, OUTPUT_PREDS_F32, None),
-      ('basic_bf16', OUTPUT_LABELS, OUTPUT_PREDS_BF16, None),
-      ('batch_size_one', OUTPUT_LABELS_BS1, OUTPUT_PREDS_BS1, None),
-      ('weighted_f16', OUTPUT_LABELS, OUTPUT_PREDS_F16, SAMPLE_WEIGHTS),
-      ('weighted_f32', OUTPUT_LABELS, OUTPUT_PREDS_F32, SAMPLE_WEIGHTS),
-      ('weighted_bf16', OUTPUT_LABELS, OUTPUT_PREDS_BF16, SAMPLE_WEIGHTS),
-  )
-  def test_rmse(self, y_true, y_pred, sample_weights):
-    """Test that `RMSE` Metric computes correct values."""
-    y_true = y_true.astype(y_pred.dtype)
-    y_pred = y_pred.astype(y_true.dtype)
-    if sample_weights is None:
-      sample_weights = np.ones_like(y_true)
-
-    metric = None
-    keras_rmse = keras.metrics.RootMeanSquaredError()
-    for labels, logits, weights in zip(y_true, y_pred, sample_weights):
-      update = metrax.RMSE.from_model_output(
-          predictions=logits,
-          labels=labels,
-          sample_weights=weights,
-      )
-      metric = update if metric is None else metric.merge(update)
-      keras_rmse.update_state(labels, logits, sample_weight=weights)
-
-    # Use lower tolerance for lower precision dtypes.
-    rtol = 1e-2 if y_true.dtype in (jnp.float16, jnp.bfloat16) else 1e-05
-    atol = 1e-2 if y_true.dtype in (jnp.float16, jnp.bfloat16) else 1e-05
-    np.testing.assert_allclose(
-        metric.compute(),
-        keras_rmse.result(),
-        rtol=rtol,
-        atol=atol,
-    )
-
-  @parameterized.named_parameters(
-      ('basic_f16', OUTPUT_LABELS, OUTPUT_PREDS_F16, None),
-      ('basic_f32', OUTPUT_LABELS, OUTPUT_PREDS_F32, None),
-      ('basic_bf16', OUTPUT_LABELS, OUTPUT_PREDS_BF16, None),
-      ('batch_size_one', OUTPUT_LABELS_BS1, OUTPUT_PREDS_BS1, None),
-      ('weighted_f16', OUTPUT_LABELS, OUTPUT_PREDS_F16, SAMPLE_WEIGHTS),
-      ('weighted_f32', OUTPUT_LABELS, OUTPUT_PREDS_F32, SAMPLE_WEIGHTS),
-      ('weighted_bf16', OUTPUT_LABELS, OUTPUT_PREDS_BF16, SAMPLE_WEIGHTS),
-  )
-  def test_rsquared(self, y_true, y_pred, sample_weights):
-    """Test that `RSQUARED` Metric computes correct values."""
-    y_true = y_true.astype(y_pred.dtype)
-    y_pred = y_pred.astype(y_true.dtype)
-    if sample_weights is None:
-      sample_weights = np.ones_like(y_true)
-
-    metric = None
-    keras_r2 = keras.metrics.R2Score()
-    for labels, logits, weights in zip(y_true, y_pred, sample_weights):
-      update = metrax.RSQUARED.from_model_output(
-          predictions=logits,
-          labels=labels,
-          sample_weights=weights,
-      )
-      metric = update if metric is None else metric.merge(update)
-
-      keras_r2.update_state(
-          labels[:, jnp.newaxis],
-          logits[:, jnp.newaxis],
-          sample_weight=weights[:, jnp.newaxis],
-      )
-
-    # Use lower tolerance for lower precision dtypes.
-    rtol = 1e-2 if y_true.dtype in (jnp.float16, jnp.bfloat16) else 1e-05
-    atol = 1e-2 if y_true.dtype in (jnp.float16, jnp.bfloat16) else 1e-05
-    np.testing.assert_allclose(
-        metric.compute(),
-        keras_r2.result(),
-        rtol=rtol,
-        atol=atol,
-    )
-
 
 if __name__ == '__main__':
-  os.environ['XLA_FLAGS'] = (
-      '--xla_force_host_platform_device_count=4'  # Use 4 CPU devices
-  )
   absltest.main()
